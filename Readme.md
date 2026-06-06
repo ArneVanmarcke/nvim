025990bd111232da8a5eec1a56ca42990862fa05
ArneVanmarcke <arne.vanmarcke2001@gmail.com>
2026-06-06 08:49:27 +0200

    Changed nvim setup to work on nvim version 12+ (On Windows)

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Neovim Setup
+
+## Windows
+
+| Tool | Command / Link |
+|------|---------------|
+| Neovim | [nvim-win64.msi](https://github.com/neovim/neovim/releases/latest/download/nvim-win64.msi) |
+| Git | `winget install -e --id Git.Git` |
+| Nvim config | `git clone https://github.com/ArneVanmarcke/nvim.git %LOCALAPPDATA%\nvim` |
+| Zig (C compiler / clangd) | `winget install -e --id zig.zig` |
+| lua_ls | [LuaLS releases](https://github.com/LuaLS/lua-language-server/releases/latest) *(not needed)* |
+| Pyright | `pip install pyright` |
+| Rust + Cargo | [win.rustup.rs](https://win.rustup.rs/) |
+| LLVM | [llvm-project releases](https://github.com/llvm/llvm-project/releases) |
+| tree-sitter-cli | `cargo install tree-sitter-cli` |
+
+---
+
+## Linux
+
+> WIP
--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -2,9 +2,27 @@ return {
 	{
 		-- Highlight, edit, and navigate code
 		'nvim-treesitter/nvim-treesitter',
+		branch = 'main',
 		dependencies = {
-			'nvim-treesitter/nvim-treesitter-textobjects',
+			{ 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'main' },
 		},
 		build = ':TSUpdate',
+		init = function()
+			-- Parser installation
+			local ensureInstalled = { 'go', 'lua', 'vimdoc', 'vim', 'bash', 'zig', 'javascript', 'python' }
+			local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+			local toInstall = vim.iter(ensureInstalled)
+				:filter(function(p) return not vim.tbl_contains(alreadyInstalled, p) end)
+				:totable()
+			require('nvim-treesitter').install(toInstall)
+
+			-- Highlighting + indentation via FileType autocmd
+			vim.api.nvim_create_autocmd('FileType', {
+				callback = function()
+					pcall(vim.treesitter.start)
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
 	}
 }
